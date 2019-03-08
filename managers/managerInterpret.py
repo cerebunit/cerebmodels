46564920210ae7c28b8ea41ce7e8e21951fd1618
Lungsi Ngwua <neuralgraphs@gmail.com>
2019-03-08 08:39:03 +0000

    added base to the named create_efel_trace_individual/overall

--- managers/managerInterpret.py
+++ managers/managerInterpret.py
@@ -30,8 +30,10 @@ class InterpretManager(object):
         return timestamps, datavalues
 
     @staticmethod
-    def create_efel_trace_individual(timestamp, datavalue):
+    def create_base_efel_trace_individual(timestamp, datavalue):
         """This is called by cls.create_efel_trace_overall
+        Input: array of times, array of values
+        Output: dictionary with keys; 'T', 'V', 'stim_start', 'stim_end'
         """
         a_trace = {}
         a_trace["V"] = datavalue
@@ -41,17 +43,20 @@ class InterpretManager(object):
         return a_trace
 
     @classmethod
-    def create_efel_trace_overall(cls, timestamps, datavalues):
-        """This is called by self.get_efel_results
+    def create_base_efel_trace_overall(cls, timestamps, datavalues):
+        """This is called by self.get_base_efel_results
+        Input: list of array of times (i.e, epochs/intervals)
+               list of array of values
+        Output: list of base_trace dictionary for each epoch
         """
-        return [ cls.create_efel_trace_individual( timestamps[i], datavalues[i] )
+        return [ cls.create_base_efel_trace_individual( timestamps[i], datavalues[i] )
                  for i in range(len(timestamps)) ]
 
     def get_efel_results(self, timestamps, datavalues, feature_name_list):
         """This returns a dictionary of { feature_name: feature_value }
         feature_name_list is list of strings, eg, ['voltage_base']
         """
-        traces = self.create_efel_trace_overall( timestamps, datavalues )
+        traces = self.create_base_efel_trace_overall( timestamps, datavalues )
         return efel.getFeatureValues( traces, feature_name_list )
 
     def gather_efel_values(self, trace_results):
@@ -63,3 +68,20 @@ class InterpretManager(object):
         """
         return [ feature_value
                  for feature_name, feature_value in trace_results.items() ]
+
+#    @staticmethod
+#    def buildon_efel_trace_individual(prebuilt_trace, **kwargs):
+#        """Adds efel features to an already built trace
+#        Eg: kwargs = {"Threshold": [ -45. ], "min_spike_height": [ -50. ]}
+#        """
+#        for feature_name, its_value in kwargs.items():
+#            prebuilt_trace["feature_name"] = its_value
+#        return prebuilt_trace
+
+#    @classmethod
+#    def extract_efel_featurevalue(cls, prebuilt_trace, feature_name, prereq_features):
+#        """Passes a list of only one trace and therefore also list of only one feature
+#        which in turn returns its feature values by extracting it (first, only one
+#        index) from list of only one feature value.
+#        """
+#        return efel.getFeatureValues( [prebuilt_trace], [feature_name] )[0][feature_name]

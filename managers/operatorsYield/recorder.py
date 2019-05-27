df05fdbbbc4fe934e4821dbd7e9c807571848dc5
Lungsi Ngwua <neuralgraphs@gmail.com>
2019-05-27 08:25:41 +0000

    recorder is Python3 compartible

--- managers/operatorsYield/recorder.py
+++ managers/operatorsYield/recorder.py
@@ -4,21 +4,37 @@ import numpy as np
 from neuron import h
 
 class Recorder(object):
-    """Operators working under RecordManager
+    """
+    ** Available methods:**
 
-    Available methods:
-    time_NEURON
-    response_voltage_NEURON
-    stimulus_individual_currents_NEURON
-    stimulus_overall_current_NEURON
+    +-------------------------------------------------+------------------------+
+    | Method name                                     | Method type            |
+    +=================================================+========================+
+    | :py:meth:`.time_NEURON`                         | static method          |
+    +-------------------------------------------------+------------------------+
+    | :py:meth:`.response_voltage_NEURON`             | static method          |
+    +-------------------------------------------------+------------------------+
+    | :py:meth:`.stimulus_individual_currents_NEURON` | static method          |
+    +-------------------------------------------------+------------------------+
+    | :py:meth:`.stimulus_overall_current_NEURON`     | static method          |
+    +-------------------------------------------------+------------------------+
+
     """
 
     @staticmethod
     def time_NEURON():
-        """
-        Use case:
-        rc = Recorder()
-        rec_t = rc.time_NEURON()
+        """Returns an array (NEURON's ``h.Vector``) of recorded time.
+
+        **Arguments:** No arguments
+
+        **Returned value:** ``h.Vector`` of recorded times (time-axis). `More about this data type. <https://www.neuron.yale.edu/neuron/static/new_doc/programming/math/vector.html>`_
+
+        **Use case:**
+
+        ``>> rc = Recorder()``
+
+        ``>> rec_t = rc.time_NEURON()``
+
         """
         recorded_time = h.Vector()
         recorded_time.record(h._ref_t)
@@ -26,12 +42,22 @@ class Recorder(object):
 
     @staticmethod
     def response_voltage_NEURON(section):
-        """
-        Use case:
-        rc = Recorder()
-        cell = Purkinje()
-        vm_soma = rc.response_voltage_NEURON(cell.soma)
-        axonNOR3 = rc.response_voltage_NEURON(cell.axonNOR3)
+        """Returns an array (NEURON's ``h.Vector``) of recorded voltage response from a given cell section.
+
+        **Arguments:** Pass a NEURON `section <https://www.neuron.yale.edu/neuron/static/new_doc/modelspec/programmatic/topology/secspec.html>`_ of the cell, assuming that the model is instantiated.
+
+        **Returned value:** ``h.Vector`` of recorded voltages. `More about this data type. <https://www.neuron.yale.edu/neuron/static/new_doc/programming/math/vector.html>`_
+
+        **Use case:**
+
+        ``>> rc = Recorder()``
+
+        ``>> cell = Purkinje() # instantiate``
+
+        ``>> vm_soma = rc.response_voltage_NEURON(cell.soma)``
+
+        ``>> axonNOR3 = rc.response_voltage_NEURON(cell.axonNOR3)``
+
         """
         recorded_voltage = h.Vector()
         recorded_voltage.record(section(0.5)._ref_v)
@@ -39,10 +65,18 @@ class Recorder(object):
 
     @staticmethod
     def stimulus_individual_currents_NEURON(stimuli):
-        """
-        Use case:
-        rc = Recorder()
-        injections = rc.stimulus_individual_currents_NEURON(stimuli)
+        """Returns a dictionary with keys in the form: "stim0", "stim1", "stim2", and so on ..., each representing an interval of current injection. The value for each key is an array (NEURON's ``h.Vector``) of recorded current injections given to a cell section.
+
+        **Arguments:** Pass a list made up of `h.Vector <https://www.neuron.yale.edu/neuron/static/new_doc/programming/math/vector.html>`_. The ``h.Vector``'s representing any of the available current types, like, ``h.IClamp``, ``h.IRamp``, etc ...
+
+        **Returned value:** {"stim0": ``h.Vector``, "stim1": ``h.Vector``, ..., "stimN": ``h.Vector``} and each respective ``h.Vector`` contains the array of amplitude of the injected current for the respective interval.
+
+        **Use case:**
+
+        ``>> rc = Recorder()``
+
+        ``>> injections = rc.stimulus_individual_currents_NEURON(stimuli)``
+
         """
         no_of_stimuli = len(stimuli)
         recorded_currents = {}
@@ -55,13 +89,27 @@ class Recorder(object):
 
     @staticmethod
     def stimulus_overall_current_NEURON(recorded_currents):
-        """
-        Use case:
-        rc = Recorder()
-        injections = rc.stimulus_individual_currents_NEURON(stimuli)
-        overallinjection = rc.stimulus_overall_current_NEURON(injections)
+        """Returns an array, NEURON's `h.Vector <https://www.neuron.yale.edu/neuron/static/new_doc/programming/math/vector.html>`_, such that all the ``h.Vector``'s from :py:meth:`.stimulus_individual_currents_NEURON` is combined/flattened into just one. That is, one trace of all the current injections (amplitudes).
 
-        NOTE: This method should only be called AFTER h.run()
+        **Arguments:** Pass the dictionary {"stim0": ``h.Vector``, "stim1": ``h.Vector``, ..., "stimN": ``h.Vector``} with each respective ``h.Vector`` containing the array of amplitude of the injected current for the respective interval.
+
+        *NOTE:* The dictionary for the argument is the returned value of :py:meth:`.stimulus_individual_currents_NEURON`.
+
+        **Returned value:** ``h.Vector`` of recorded currents. `More about this data type. <https://www.neuron.yale.edu/neuron/static/new_doc/programming/math/vector.html>`_
+
+        **Use case:**
+
+        ``>> rc = Recorder()``
+
+        ``>> injections = rc.stimulus_individual_currents_NEURON(stimuli)``
+
+        ``>> overallinjection = rc.stimulus_overall_current_NEURON(injections)``
+
+        *NOTE:*
+
+        * This method should only be called AFTER ``h.run()``
+        * Flattening of the currents is done by appending into the value of "stim0", i.e., appending into its ``h.Vector``.
+
         """
         no_of_stimuli = len(recorded_currents)
         for i in range(no_of_stimuli - 1):

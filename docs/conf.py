a55b58b5b01cd7c8b15540d87cbc4bec305ef516
Lungsi Ngwua <neuralgraphs@gmail.com>
2019-05-28 08:40:21 +0000

    v1.0.0-SNAPSHOT -> v0.3.0-SNAPSHOT

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = u'Lungsi'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'1.0.0-SNAPSHOT'
+release = u'0.3.0-SNAPSHOT'
 
 
 # -- General configuration ---------------------------------------------------

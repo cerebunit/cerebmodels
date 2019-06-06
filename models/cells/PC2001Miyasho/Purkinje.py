4a3ad78b44dc177d885ec4c4013c08da57911150
Lungsi Ngwua <neuralgraphs@gmail.com>
2019-06-06 15:04:11 +0000

    added SciUnit wrapped PC2001Miyahso

---
+++ models/cells/PC2001Miyasho/Purkinje.py
@@ -0,0 +1,18 @@
+#Template of the Purkinje cell model (1088 compartments), Miyasho et al. 2001)
+#Templating by Lungsi 2019 based on ~/PC2001Miyaho/purkinje.hoc
+from neuron import h
+#from pdb import set_trace as breakpoint
+
+class Purkinje(object):
+    """Multi-compartment cell
+    """
+    def __init__(self):
+        h.xopen("purkinje.hoc") # Within it parameters are defined and
+        # its geometry is set by loading granuel.nrn
+
+        # There are thirteen compartments and the following are chosen as
+        # attributes to this python class for potential recording
+        self.soma = h.soma
+        self.dend_sm = h.SmoothDendrite # len(h.SmoothDendrite) -> 85
+        self.dend_sp = h.SpinyDendrite  # len(h.SpinyDendrite) -> 1002
+

47b27eaa97f7f4bc29bbdecae8c039c8419e8eea
Lungsi Ngwua <neuralgraphs@gmail.com>
2019-06-06 15:26:26 +0000

    added SciUnit wrapped PC2003Khaliq

---
+++ models/cells/PC2003Khaliq/Purkinje.py
@@ -0,0 +1,15 @@
+#Template of the Purkinje cell model (single compartment), Khaliq et al. 2003)
+#Templating by Lungsi 2019 based on ~/PC2003Khaliq/resurgesim.hoc
+from neuron import h
+#from pdb import set_trace as breakpoint
+
+class Purkinje(object):
+    """Single-compartment cell
+    """
+    def __init__(self):
+        h.xopen("resurgesim.hoc")
+
+        # Since it has only one compartment the attribute for this
+        # python class for recording is just soma
+        self.soma = h.soma
+

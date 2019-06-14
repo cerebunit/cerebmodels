993623ebcfaed161a75da17d6150da2c79a5b9b7
Lungsi Ngwua <neuralgraphs@gmail.com>
2019-06-14 08:52:26 +0000

    GoC -> GrC in model description

--- models/cells/modelGrC2011Souza.py
+++ models/cells/modelGrC2011Souza.py
@@ -31,7 +31,7 @@ class GranuleCell( sciunit.Model,
         self.regions = {"soma": 0.0, }
         # -----------attributed inheritance from sciunit.Model--------------
         self.name = "Souza and Schutter 2011 model of GranuleCell"
-        self.description = "Souza & Schutter 2011 model of GranuleCell (GrC) and published in 10.1186/2042-1001-1-7 This is a single compartment model; it is a modification of Diwakar et al. 2009 multicompartment model. It was used as the component for GoC in the network the authors constructed. This model is the SciUnit wrapped version of the NEURON model in modelDB accession # 139656."
+        self.description = "Souza & Schutter 2011 model of GranuleCell (GrC) and published in 10.1186/2042-1001-1-7 This is a single compartment model; it is a modification of Diwakar et al. 2009 multicompartment model. It was used as the component for GrC in the network the authors constructed. This model is the SciUnit wrapped version of the NEURON model in modelDB accession # 139656."
         #
         ### =================== Instantiate cell template ====================
         sm.lock_and_load_model_libraries(modelscale=self.modelscale,

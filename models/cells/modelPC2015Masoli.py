02f867129441c5cf6a99ba8eda37e69f08f7c55d
Lungsi Ngwua <neuralgraphs@gmail.com>
2019-07-05 14:51:15 +0000

    SciUnit wrapped PC2015Masoli

--- models/cells/modelPC2015Masoli.py
+++ models/cells/modelPC2015Masoli.py
@@ -4,11 +4,20 @@ pwd = os.getcwd()
 path_to_files = pwd + os.sep + "models" + os.sep + "cells" + os.sep + \
                 "PC2015Masoli" + os.sep
 
-from managers.simulation import SimulationManager as sm
 from models.cells.PC2015Masoli.Purkinje import Purkinje
+from executive import ExecutiveControl
+from managers.simulation import SimulationManager as sm
+from managers.read import ReadManager as rm
+from managers.signalprocessing import SignalProcessingManager as spm
+
+import sciunit
+from cerebunit.capabilities.cells.response import ProducesElectricalResponse
+from cerebunit.capabilities.cells.measurements import ProducesEphysMeasurement
 
 
-class PurkinjeCell(object):
+class PurkinjeCell( sciunit.Model,
+                    ProducesElectricalResponse,
+                    ProducesEphysMeasurement ):
     """USE CASE:
     """
 
@@ -16,44 +25,81 @@ class PurkinjeCell(object):
     #uuid = "22dc8fd3-c62b-4e07-9e47-f5829e038d6d"
 
     def __init__(self):
-        ### =====================Instantiate the cell======================
-        #path_to_files = pwd + os.sep + "models" + os.sep + "cells" + \
-        #                os.sep + "PC2015Masoli" + os.sep
-        #os.chdir(path_to_files) # change to path_to_files
-        #self.cell = Purkinje()
-        #os.chdir(pwd)           # reset to start directory
-        # ------specify cell-regions from with response are recorded-------
-        self.regions = {"soma": 0.0, "axonAIS": 0.0, "axonNOR3": 0.0}
-        ### ===============================================================
-        #
-        ### =====================Essential Attributes======================
+        ### ===================== Descriptive Attributes ======================
         self.modelscale = "cells"
         self.modelname = "PC2015Masoli"
+        # ------specify cell-regions from with response are recorded-------
+        self.regions = {"soma": 0.0}#, "axonAIS": 0.0, "axonNOR3": 0.0}
         # -----------attributed inheritance from sciunit.Model--------------
         # pc.name defaults to class name, i.e, PurkinjeCell
         self.name = "Masoli et al. 2015 model of PurkinjeCell"
         self.description = "Masoli et al. 2015 model of PurkinjeCell (PC) and published in 10.3389/fncel.2015.00047 This is general PC model unlike special Z+ or Z- models. The model is based on adult (P90 or 3 months) Guinea pig. PC in younger ones are not mature and they grow until P90. This model is the SciUnit wrapped version of the NEURON model in modelDB accession # 229585."
         #
+        ### =================== Instantiate cell template ====================
         sm.lock_and_load_model_libraries(modelscale=self.modelscale,
                                          modelname=self.modelname)
         os.chdir(path_to_files)
         self.cell = Purkinje()
         os.chdir(pwd)
         ### ===============================================================
+        self.fullfilename = "nil"
+        self.prediction = "nil"
 
     # =======================================================================
     # +++++++++++++++++++++++ MODEL CAPABILITIES ++++++++++++++++++++++++++++
     # =======================================================================
 
     # --------------------- produce_voltage_response ------------------------
-    def produce_voltage_response():
+    def produce_voltage_response(self, **kwargs):
         """generic/essential model response
 
-        Argument:
-        sm -- instantiated SimulationManager()
+        **Keyword Arguments:**
+
+        kwargs = { "parameters": dictionary with keys,
+                   "stimparameters": None or dictionary with keys "type" and "stimlist",
+                   "onmodel": instantiated model }
+
         """
-        sm.engage_NEURON()
-        
+        print("Simulation produce_voltage_response starting ...")
+        ec = ExecutiveControl() # only works when in ~/cerebmodels
+        model = ec.launch_model( parameters = kwargs["parameters"],
+                                 stimparameters = kwargs["stimparameters"],
+                                 stimloc = kwargs["stimloc"],
+                                 onmodel = kwargs["onmodel"], mode = "raw" )
+        print("File saving ...")
+        fullfilename = ec.save_response()
+        setattr(model, "fullfilename", fullfilename)
+        print("File saved.")
+        print("Simulation produce_voltage_response Done.")
+        return model
+
+    # ----------------------- produce_restingVm ----------------------------- 
+    def produce_restingVm(self, **kwargs):
+        """
+        kwargs = { "parameters": dictionary with keys,
+                   "stimparameters": dictionary with keys "type" and "stimlist",
+                   "onmodel": instantiated model }
+        """
+        print("Sim produce_restingVm starting ...")
+        ec = ExecutiveControl() # only works when in ~/cerebmodels
+        model = ec.launch_model( parameters = kwargs["parameters"],
+                                 stimparameters = kwargs["stimparameters"],
+                                 stimloc = kwargs["stimloc"], onmodel = kwargs["onmodel"],
+                                 capabilities = {"model": "produce_voltage_response",
+                                                 "vtest": ProducesElectricalResponse},
+                                 mode="capability" )
+        nwbfile = rm.load_nwbfile(model.fullfilename)
+        orderedepochs = rm.order_all_epochs_for_region(nwbfile=nwbfile, region="soma")
+        timestamps_over_epochs = [ rm.timestamps_for_epoch( orderedepochs[i] )
+                                   for i in range(len(orderedepochs)) ]
+        data_over_epochs = [ rm.data_for_epoch( orderedepochs[i] )
+                                   for i in range(len(orderedepochs)) ]
+        baseVms = spm.distill_Vm_pre_epoch( timestamps = timestamps_over_epochs,
+                                            datavalues = data_over_epochs )
+        setattr(model, "prediction", baseVms)
+        print("Simulation produce_restingVm Done.")
+        return model
+
     # ----------------------- produce_spike_train ---------------------------
     def produce_spike_train(self, **kwargs):
         """

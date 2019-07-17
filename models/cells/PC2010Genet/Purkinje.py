9c892bbdb1450a34e193462be8dbd761c7aeb09e
Lungsi Ngwua <neuralgraphs@gmail.com>
2019-07-17 09:23:12 +0000

    picks one dendrite each; smooth/85 and spiny/86

--- models/cells/PC2010Genet/Purkinje.py
+++ models/cells/PC2010Genet/Purkinje.py
@@ -2,6 +2,7 @@
 #Templating by Lungsi 2019 based on ~/PC2010Genet/full_CP.hoc
 from neuron import h
 #from pdb import set_trace as breakpoint
+from random import randint
 
 class Purkinje(object):
     """Multi-compartment cell
@@ -12,11 +13,14 @@ class Purkinje(object):
         # There are 1088 compartments and the following are chosen as
         # attributes to this python class for potential recording
         self.soma = h.soma
-        self.dend_sm = h.sm      # len(h.sm) -> 85
+        #
+        dend_sm = h.sm      # len(h.sm) -> 85
         #self.dend_sp1 = h.spn1 # len() -> 1
         #but there are 86 such spiny dendrites and unlike h.sm they are separate
-        self.dend_sp = []
+        dend_sp = []
         for i in range(1, 86+1):
-            self.dend_sp.append( getattr(h,"spn"+str(i))[0] ) # since nseg = 1
+            dend_sp.append( getattr(h,"spn"+str(i))[0] ) # since nseg = 1
+        #
+        self.dend_sm = dend_sm[ randint(0, len(dend_sm)-1) ]
+        self.dend_sp = dend_sp[ randint(0, len(dend_sp)-1) ]
         
-
